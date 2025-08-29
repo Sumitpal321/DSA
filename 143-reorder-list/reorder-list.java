40cc730bd1801fe8bacc3165b0e2a999ca265926
Sumitpal321 <Sumitkumar71496@gmail.com>
2025-08-29 19:31:47 +0530

    Time: 2 ms (85.11%) | Memory: 48.2 MB (70.96%) - LeetSync

---
+++ 143-reorder-list/reorder-list.java
@@ -0,0 +1,39 @@
+class Solution {
+    public ListNode reverseList(ListNode newHead){
+        ListNode prev = null;
+        ListNode curr =newHead;
+        while(curr!=null){
+            ListNode Next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = Next;
+        }
+        return prev;
+    }
+    public void reorderList(ListNode head) {
+        if(head == null || head.next == null) return ;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null &&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode second = reverseList(slow.next);
+        slow.next = null;
+        ListNode dummy = new ListNode(-1);
+        ListNode first = head;
+        ListNode temp = dummy;
+        while(first!=null && second!=null){
+            temp.next = first;
+            first = first.next;
+            temp = temp.next;
+
+            temp.next = second;
+            second = second.next;
+            temp = temp.next;
+        }
+        if(first!=null) temp.next = first;
+        if(second!=null) temp.next = second;
+
+    }
+}
