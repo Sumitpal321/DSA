8f15f6492f1026e8e8196fd836c4121c00b4f0c1
Sumitpal321 <Sumitkumar71496@gmail.com>
2026-03-20 17:56:41 +0530

    Time: 0 ms (100.00%) | Memory: 46.7 MB (45.21%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -1,56 +1,29 @@
 class Solution {
-    public Node deepcopy(Node head){
-        Node head2 = new Node(head.val);
-        Node temp1 = head.next;
-        Node temp2 = head2;
-        while(temp1!=null){
-            Node temp = new Node(temp1.val);
-            temp2.next = temp;
-            temp2 = temp2.next;
-            temp1 = temp1.next;
-        }
-        return head2;
-    }
-    public void connectAlternatively(Node head, Node head2){
-        Node t1 = head;
-        Node t2 = head2;
-        Node dummy = new Node(-1);
-        Node t = dummy;
-        while(t1!=null && t2!=null){
-            t.next = t1;
-            t1 = t1.next;
-            t = t.next;
-            t.next = t2;
-            t2 = t2.next;
-            t = t.next;
-        }
-    }
-    public void assignRandomPointers(Node head, Node head2){
-        Node t1 = head;
-        Node t2 = head2;
-        while(t1!=null){
-            t2 = t1.next;
-            if(t1.random!=null) t2.random = t1.random.next;
-            t1 = t1.next.next;
-        }
-    }
-    public void split(Node head, Node head2){
-        Node t1 = head;
-        Node t2 = head2;
-        while(t1!=null){
-            t1.next = t2.next;
-            t1 = t1.next;
-            if(t1==null) break;
-            t2.next = t1.next;
-            t2 = t2.next;
-        }
-    }
     public Node copyRandomList(Node head) {
-        if(head==null) return null;
-        Node head2 = deepcopy(head);
-        connectAlternatively(head, head2);
-        assignRandomPointers(head, head2);
-        split(head, head2);
+        if(head == null) return null;
+        Node head2 = new Node(head.val);
+        Node temp2 = head2;
+        Node temp = head.next;
+        while(temp!=null){
+            Node dup = new Node(temp.val);
+            temp2.next = dup;
+            temp2 = dup;
+            temp = temp.next;
+        }
+        HashMap<Node , Node> map = new HashMap<>();
+        temp2 = head2;
+        temp = head;
+        while(temp!=null){
+            map.put(temp, temp2);
+            temp = temp.next;
+            temp2 = temp2.next;
+        }
+        for(Node Original : map.keySet()){
+            Node duplicate = map.get(Original);
+            if(Original.random!=null){
+                duplicate.random = map.get(Original.random);
+            }
+        }
         return head2;
     }
 }
