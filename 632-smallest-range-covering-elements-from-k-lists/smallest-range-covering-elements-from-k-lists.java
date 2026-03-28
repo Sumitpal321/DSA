ffe5b8f9060ef1b4ead746b3a98c1f1ed84a01f7
Sumitpal321 <Sumitkumar71496@gmail.com>
2026-03-28 21:34:54 +0530

    Time: 99 ms (54.77%) | Memory: 86.9 MB (16.85%) - LeetSync

---
+++ 632-smallest-range-covering-elements-from-k-lists/smallest-range-covering-elements-from-k-lists.java
@@ -0,0 +1,39 @@
+class Solution {
+    public class Triplet implements Comparable<Triplet>{
+        int ele;
+        int row;
+        int col;
+        Triplet(int ele, int row, int col){
+            this.ele = ele;
+            this.row = row;
+            this.col = col;
+        }
+        public int compareTo(Triplet t){
+            return this.ele - t.ele;
+        }
+    }
+    public int[] smallestRange(List<List<Integer>> nums) {
+        int [] ans = {0, Integer.MAX_VALUE};
+        PriorityQueue<Triplet> pq = new PriorityQueue<>();
+        int max = Integer.MIN_VALUE;
+        int k = nums.size();
+        for(int i=0;i<k;i++){
+            int ele = nums.get(i).get(0);
+            pq.add(new Triplet(ele, i, 0));
+            max = Math.max(ele, max);
+        }
+        while(true){
+            Triplet top = pq.remove();
+            int ele = top.ele, row = top.row, col = top.col;
+            if(max - ele < ans[1]-ans[0]){
+                ans[0]=ele;
+                ans[1]=max;
+            }
+            if(col==nums.get(row).size()-1) break;
+            int next = nums.get(row).get(col+1);
+            max = Math.max(max , next);
+            pq.add(new Triplet(next, row, col+1));
+        }
+        return ans;
+    }
+}
