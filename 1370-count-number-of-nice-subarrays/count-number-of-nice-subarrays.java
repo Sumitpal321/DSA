88b5501b4e7857b098eb00d6e8ff712db2e62a4b
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-04-07 06:34:33 +0530

    Time: 46 ms (7.36%) | Memory: 61.2 MB (5.31%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,22 +1,17 @@
 class Solution {
     public int numberOfSubarrays(int[] arr, int k) {
-        int n = arr.length, i = 0, j = 0, a = 0, b = 0, count = 0, k2=0;
-        while(i<n && arr[i]%2==0) i++;
-        while(j<n && k2<k) if(arr[j++]%2!=0) k2++;
-        if(k2<k) return 0;
-        j--;
-        b = j + 1;
-        while(b<n && arr[b]%2==0) b++;
-        b--;
-        while(b<n){
-            count += (i-a+1) * (b-j+1);
-            a=i+1;
-            i++;
-            while(i<n && arr[i]%2==0) i++;
-            j = b+1;
-            b = j + 1;
-            while(b<n && arr[b]%2==0) b++;
-            b--;
+        int n = arr.length, count = 0;
+        for(int i=0;i<n;i++) arr[i]%=2;
+        for(int i=1;i<n;i++) arr[i] += arr[i-1];
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0;i<n;i++){
+            if(!map.containsKey(arr[i])) map.put(arr[i], i);
+            int a = 0;
+            if(map.containsKey(arr[i]-k)) a = map.get(arr[i]-k);
+            int b = 0;
+            if(map.containsKey(arr[i]-k+1)) b = map.get(arr[i]-k+1);
+            if(arr[i]==k) count += (b-a+1);
+            if(arr[i]>k) count += (b-a);
         }
         return count;
     }
