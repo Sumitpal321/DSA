4ecf3f4747461eb54dd7e675ecb1eb976657dbb6
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-04-11 13:44:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (47.48%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if(n<2) return nums[0];
+
+        int[] skiplastHouse = new int[n-1];
+        int[] skipfirstHouse = new int[n-1];
+
+        for(int i=0;i<n-1;i++){
+            skiplastHouse[i] = nums[i];
+            skipfirstHouse[i] = nums[i+1];
+        }
+
+        int lootskippinglast = robhelper(skiplastHouse);
+        int lootskippingfirst = robhelper(skipfirstHouse);
+
+        return Math.max(lootskippinglast, lootskippingfirst);
+    }
+
+    public int robhelper(int[] nums){
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        if(n>1) dp[1] = Math.max(nums[0], nums[1]);
+        for(int i=2;i<n;i++){
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
+    }
+}
