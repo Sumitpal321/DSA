3dca0c622ae4393c5f290ddcdf97d4fb80f354fb
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-04-18 21:00:57 +0530

    Time: 18 ms (93.08%) | Memory: 42.8 MB (97.65%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.java
+++ 1250-longest-common-subsequence/longest-common-subsequence.java
@@ -1,22 +1,17 @@
 class Solution {
-    static int[][] dp;
-    public int lcs(int i, int j, StringBuilder a, StringBuilder b) {
-        if(i<0 || j<0) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)){
-            return dp[i][j] = 1 + lcs(i-1, j-1, a, b);
-        }
-        else
-            return dp[i][j] = Math.max(lcs(i-1,j,a,b), lcs(i,j-1,a,b));
-    }
-    public int longestCommonSubsequence(String text1, String text2){
-        StringBuilder a = new StringBuilder(text1);
-        StringBuilder b = new StringBuilder(text2);
+    public int longestCommonSubsequence(String a, String b) {
         int m = a.length(), n = b.length();
-        dp = new int[m][n];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<dp[0].length;j++) dp[i][j] = -1;
+        int [][] dp = new int[2][n+1];
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(a.charAt(i-1) == b.charAt(j-1))
+                dp[1][j] = 1 + dp[0][j-1];
+                else{
+                    dp[1][j] = Math.max(dp[1][j-1], dp[0][j]);
+                }
+            }
+            for(int j=0;j<=n;j++) dp[0][j] = dp[1][j];
         }
-        return lcs(m-1, n-1, a, b);
+        return dp[1][n];
     }
 }
