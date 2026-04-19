05aa663ddf2c8a7cb27377592c7f5ded212ec564
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-04-19 14:26:16 +0530

    Time: 20 ms (78.96%) | Memory: 46.6 MB (88.07%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,30 +1,20 @@
 class Solution {
-    public String reverse(String s){
-        StringBuilder sb = new StringBuilder(s);
-        sb.reverse();
-        s = sb.toString();
-        return s;
-    }
-    public int solve(int i, int j, StringBuilder a, StringBuilder b, int[][] dp){
-        if(i<0 || j<0) return 0;
+     public int lps(String s, int i, int j, int [][] dp){
+        if(i>j) return 0;
+        if(i==j) return 1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)){
-            return dp[i][j] = 1 + solve(i-1, j-1, a, b, dp);
+        if(s.charAt(i) == s.charAt(j)){
+            return dp[i][j] = 2 + lps(s, i+1, j-1, dp);
         }
-        else return dp[i][j] = Math.max(solve(i-1, j, a, b, dp), solve(i, j-1, a, b, dp));
-    }
-    public int lcs(String text1, String text2){
-        StringBuilder a = new StringBuilder(text1);
-        StringBuilder b = new StringBuilder(text2);
-        int m = a.length(); int n = b.length();
-        int [][] dp = new int[m][n];
-        for(int i=0;i<dp.length;i++){
-            for(int j=0;j<n;j++) dp[i][j] = -1;
-        }
-        return solve(m-1, n-1, a, b, dp);
+        else return dp [i][j] = Math.max(lps(s, i+1, j, dp), lps(s, i, j-1, dp));
+
     }
     public int longestPalindromicsubsequence(String s){
-        return lcs(s, reverse(s));
+        int [][] dp = new int[s.length()][s.length()];
+        for(int[] row : dp){
+            Arrays.fill(row, -1);
+        }
+        return lps(s, 0, s.length()-1, dp);
     }
     public int minInsertions(String s) {
         return s.length() - longestPalindromicsubsequence(s);
