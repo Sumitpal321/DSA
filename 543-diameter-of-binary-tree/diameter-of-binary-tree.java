f2b79d21248c63421be6d8881e406316b17c45cd
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-04-29 20:56:47 +0530

    Time: 1 ms (42.12%) | Memory: 47.1 MB (26.33%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,16 +1,15 @@
 class Solution {
-    static int maxDia;
-    public int levels(TreeNode root){
-        if(root==null) return 0;
-        int leftlevel = levels(root.left);
-        int rightlevel = levels(root.right);
-        int dia = leftlevel + rightlevel;
-        maxDia = Math.max(dia, maxDia);
-        return 1 + Math.max(leftlevel, rightlevel);
+    public int levels(TreeNode root, int [] dia){
+        if(root == null) return 0;
+        int leftlevels = levels(root.left, dia);
+        int rightlevels = levels(root.right, dia);
+        int path = leftlevels + rightlevels;
+        dia[0] = Math.max(dia[0], path);
+        return 1 + Math.max(leftlevels, rightlevels);
     }
-    public int diameterOfBinaryTree(TreeNode root) {
-        maxDia = 0;
-        levels(root);
-        return maxDia;
+    public int diameterOfBinaryTree(TreeNode root){
+        int [] dia = {0};
+        levels(root, dia);
+        return dia[0];
     }
 }
