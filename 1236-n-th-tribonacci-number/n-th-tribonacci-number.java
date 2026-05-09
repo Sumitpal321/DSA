54c4fd2bc3092818c2bfe961440ea9e81ad5f7a7
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-05-09 09:26:42 +0530

    Time: 0 ms (100.00%) | Memory: 42 MB (52.19%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,20 +1,14 @@
 class Solution {
-    int [] dp;
-    public int find(int n){
+    public int find(int n, int [] dp){
         if(n==0) return 0;
         if(n==1 || n==2) return 1;
-
         if(dp[n]!=-1) return dp[n];
-        dp[n] = find(n-1) + find(n-2) + find(n-3);
-
+        dp[n] = find(n-1, dp) + find(n-2, dp) + find(n-3, dp);
         return dp[n];
-
     }
     public int tribonacci(int n) {
-        dp = new int[n+1];
-
+        int[] dp = new int[n+1];
         Arrays.fill(dp, -1);
-
-        return find(n);
+        return find(n, dp);
     }
 }
