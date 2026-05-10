885180e8e2c7c5ed87adf5f8d319d395c8e27bdd
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-05-10 15:35:27 +0530

    Time: 192 ms (11.80%) | Memory: 44.9 MB (25.40%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,31 +1,21 @@
 class Solution {
     public boolean isPerfect(int n){
-        int sqrt = (int)(Math.sqrt(n));
+        int sqrt = (int) (Math.sqrt(n));
         return (sqrt*sqrt == n);
     }
-    // public int minsquare(int n, int[] dp){
-    //     if(isPerfect(n)) return 1;
-    //     if(dp[n]!=-1) return dp[n];
-    //     int min = Integer.MAX_VALUE;
-    //     for(int i=1;i*i<n;i++){
-    //         int count = minsquare(i*i,dp) + minsquare(n-i*i,dp);
-    //         min = Math.min(min, count);
-    //     }
-    //     return dp[n] = min;
-    // }
-    public int numSquares(int n) {
-        int [] dp = new int[n+1];
-        for(int i=1;i<=n;i++){
-            if(isPerfect(i)) dp[i] = 1;
-            else{
-                int min = Integer.MAX_VALUE;
-                for(int j=1;j*j<=i;j++){
-                    int count = dp[j*j] + dp[i-j*j];
-                    min = Math.min(count, min);
-                }
-                dp[i] = min;
-            }
+    public int minSquare(int n, int[] dp){
+        if(isPerfect(n)) return 1;
+        if(dp[n]!=-1) return dp[n];
+        int min = Integer.MAX_VALUE;
+        for(int i=1;i*i<=n;i++){
+            int count = minSquare(i*i, dp) + minSquare(n-i*i, dp);
+            min = Math.min(min, count);
         }
-        return dp[n];
+        return dp[n] = min;
+    }
+    public int numSquares(int n) {
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return minSquare(n, dp);
     }
 }
