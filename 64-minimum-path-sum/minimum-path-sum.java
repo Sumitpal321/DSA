751ba1e18649c8cffc86521b6f2eaa1f73ab9d52
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-05-10 10:18:33 +0530

    Time: 4 ms (62.11%) | Memory: 50.7 MB (11.44%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -1,21 +1,14 @@
 class Solution {
     public int minPathSum(int[][] arr) {
-        int m = arr.length;
-        int n = arr[0].length;
-        int[][] dp = new int[m+1][n+1];
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                dp[i][j] = -1;
+        int m = arr.length, n = arr[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                else if(i==0) arr[i][j] += arr[i][j-1];
+                else if(j==0) arr[i][j] += arr[i-1][j];
+                else arr[i][j] += Math.min(arr[i][j-1], arr[i-1][j]);
             }
         }
-        return paths(m-1, n-1,arr,dp);
-    }
-    public int paths(int m, int n,int [][] arr, int[][] dp){
-        if(m<0 || n<0) return Integer.MAX_VALUE;
-        if(m==0 && n==0) return arr[0][0];
-        if(dp[m][n]!=-1) return dp[m][n];
-        int rightways = paths(m, n-1, arr, dp);
-        int downways = paths(m-1, n, arr, dp);
-        return dp[m][n] = arr[m][n] + Math.min(rightways, downways);
+        return arr[m-1][n-1];
     }
 }
