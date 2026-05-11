1cc3adef5e9310715b183308bfba03765e8406f1
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-05-11 18:37:36 +0530

    Time: 3 ms (11.95%) | Memory: 46.7 MB (95.58%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,15 +1,22 @@
 class Solution {
-    public int levels(TreeNode root, int [] dia){
-        if(root == null) return 0;
-        int leftlevels = levels(root.left, dia);
-        int rightlevels = levels(root.right, dia);
-        int path = leftlevels + rightlevels;
-        dia[0] = Math.max(dia[0], path);
-        return 1 + Math.max(leftlevels, rightlevels);
+    static Map<TreeNode, Integer> dp;
+    public int levels(TreeNode root){
+        if(root==null) return 0;
+        if(dp.containsKey(root)) return dp.get(root);
+        int leftlevels = levels(root.left);
+        int rightlevels = levels(root.right);
+        dp.put(root, 1 + Math.max(leftlevels , rightlevels));
+        return dp.get(root);
     }
-    public int diameterOfBinaryTree(TreeNode root){
-        int [] dia = {0};
-        levels(root, dia);
-        return dia[0];
+    public int diameter(TreeNode root){
+        if(root==null) return 0;
+        int mydia = levels(root.left) + levels(root.right);
+        int leftDia = diameter(root.left);
+        int rightDia = diameter(root.right);
+        return Math.max(mydia, Math.max(leftDia, rightDia));
+    }
+    public int diameterOfBinaryTree(TreeNode root) {
+        dp = new HashMap<>();
+        return diameter(root);
     }
 }
