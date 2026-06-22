9f2e0d2ffca8900665c9d259d0469d17760b2dd5
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-06-22 17:12:08 +0530

    Time: 1 ms (87.69%) | Memory: 45.5 MB (63.14%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,29 +1,29 @@
 class Solution {
-    public void helper(int[] nums, List<Integer> ds, boolean[] isValid, List<List<Integer>> ans){
+    public void helper(int[] nums, int idx, List<List<Integer>> ans){
         int n = nums.length;
-        if(ds.size()==n){
+        if(idx == n-1){
             List<Integer> list = new ArrayList<>();
-            for(int i=0;i<ds.size();i++){
-                list.add(ds.get(i));
+            for(int i=0;i<=n-1;i++){
+                list.add(nums[i]);
             }
             ans.add(list);
             return;
         }
-        for(int i=0;i<nums.length;i++){
-            if(isValid[i]==false){
-                ds.add(nums[i]);
-                isValid[i] = true;
-                helper(nums, ds, isValid, ans);
-                isValid[i] = false;
-                ds.remove(ds.size()-1);
-            }
+        for(int i=idx;i<n;i++){
+           swap(i, idx, nums);
+           helper(nums, idx+1, ans);
+           swap(i, idx, nums);
         }
+    }
+
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> ds = new ArrayList<>();
-        boolean[] isValid = new boolean[nums.length];
-        helper(nums, ds, isValid, ans);
+        helper(nums, 0, ans);
         return ans;
     }
 }
