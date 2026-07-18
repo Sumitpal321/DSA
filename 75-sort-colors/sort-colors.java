74e565316b20f6ffc1a53ecc1242f6ff11a7345c
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-07-18 16:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (77.83%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,22 +1,16 @@
 class Solution {
     public void sortColors(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> ans = new ArrayList<>();
+        int noz = 0;
+        int noo = 0;
         for(int i=0;i<n;i++){
-            if(nums[i] == 0) ans.add(0);
+            if(nums[i] == 0) noz++;
+            if(nums[i] == 1) noo++;
         }
         for(int i=0;i<n;i++){
-            if(nums[i]==1){
-                ans.add(1);
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(nums[i] == 2){
-                ans.add(2);
-            }
-        }
-        for(int i=0;i<n;i++){
-            nums[i] = ans.get(i);
+            if(i<noz) nums[i] = 0;
+            else if(i < noz+noo) nums[i] = 1;
+            else nums[i] = 2;
         }
     }
 }
