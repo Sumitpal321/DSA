7932dcace1f10731e68816d4749cf0e46614d365
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-07-19 08:21:08 +0530

    Time: 9 ms (61.35%) | Memory: 48.8 MB (60.42%) - LeetSync

--- 324-wiggle-sort-ii/wiggle-sort-ii.java
+++ 324-wiggle-sort-ii/wiggle-sort-ii.java
@@ -2,14 +2,12 @@ class Solution {
     public void wiggleSort(int[] nums) {
         int n = nums.length;
         int[] ans = new int[n];
-
+        int left = (n-1)/2;
+        int right = n-1;
         Arrays.sort(nums);
 
-        int left = (n-1) / 2;
-        int right = n-1;
-
         for(int i=0;i<n;i++){
-            if(i%2 == 0){
+            if(i % 2 == 0){
                 ans[i] = nums[left--];
             }
             else ans[i] = nums[right--];
