b7369950f0fa81d5551c140ad576ec61f7a1603c
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-07-21 19:17:02 +0530

    Time: 0 ms (100.00%) | Memory: 46.6 MB (68.31%) - LeetSync

---
+++ 898-transpose-matrix/transpose-matrix.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] transpose(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        int[][] res = new int[n][m];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                res[j][i] = arr[i][j];
+            }
+        }
+        return res;
+    }
+}
