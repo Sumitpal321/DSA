bee2c083c42a5553a14ff9b5cbaa9eb225c052cf
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-07-23 11:28:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (86.33%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,20 +1,21 @@
 class Solution {
+    public void swap(int[][] arr, int r1, int c1, int r2, int c2){
+        int temp = arr[r1][c1];
+        arr[r1][c1] = arr[r2][c2];
+        arr[r2][c2] = temp;
+    }
     public void rotate(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
         for(int i=0;i<m;i++){
             for(int j=i;j<n;j++){
-                int temp = arr[i][j];
-                arr[i][j] = arr[j][i];
-                arr[j][i] = temp;
+                swap(arr, i, j, j, i);
             }
         }
         for(int i=0;i<m;i++){
             int a =0; int b = n-1;
             while(a<b){
-                int temp = arr[i][a];
-                arr[i][a] = arr[i][b];
-                arr[i][b] = temp;
+                swap(arr, i, a, i, b);
                 a++;
                 b--;
             }
