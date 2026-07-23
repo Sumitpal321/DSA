b451da25027b0b47e2925c5bb943d63cd5287c4e
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-07-23 11:22:06 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.53%) - LeetSync

---
+++ 48-rotate-image/rotate-image.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int m = arr.length;
+        int n = arr[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=i;j<n;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        for(int i=0;i<m;i++){
+            int a =0; int b = n-1;
+            while(a<b){
+                int temp = arr[i][a];
+                arr[i][a] = arr[i][b];
+                arr[i][b] = temp;
+                a++;
+                b--;
+            }
+        }
+    }
+}
