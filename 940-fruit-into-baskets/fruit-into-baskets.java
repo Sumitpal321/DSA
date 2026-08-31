7e5873aa3be7ed21a212f2b9e091a046efc04b38
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-08-31 10:58:08 +0530

    Time: 52 ms (83.77%) | Memory: 69.8 MB (80.60%) - LeetSync

--- 940-fruit-into-baskets/fruit-into-baskets.java
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -1,28 +1,30 @@
 class Solution {
-    public int totalFruit(int[] arr) {
-        int n = arr.length;
-        int i = 0;
-        int j = 0;
-        int maxFruit = 0;
+    public int totalFruit(int[] nums) {
+        int n = nums.length;
+        int i=0;
+        int maxwindow = 0;
         HashMap<Integer, Integer> map = new HashMap<>();
-        while(j<n){
-            if(map.containsKey(arr[j])){
-                int freq = map.get(arr[j]);
-                map.put(arr[j], freq+1);
+        for(int j=0;j<n;j++){
+            if(map.containsKey(nums[j])){
+                int freq = map.get(nums[j]);
+                map.put(nums[j], freq+1);
             }
-            else map.put(arr[j], 1);
+            else map.put(nums[j], 1);
 
-            while(map.size()>2){
-                int freq = map.get(arr[i]);
-                if(freq == 1) map.remove(arr[i]);
-                else map.put(arr[i], freq-1);
+            while(map.size() > 2){
+                int freq = map.get(nums[i]);
+
+                if(freq == 1){
+                    map.remove(nums[i]);
+                }
+                else{
+                    map.put(nums[i], freq-1);
+                }
                 i++;
             }
-            int len = j-i+1;
-            maxFruit = Math.max(len, maxFruit);
-            j++;
+            int window_len = j-i+1;
+            maxwindow = Math.max(window_len, maxwindow);
         }
-        return maxFruit;
-
+        return maxwindow;
     }
 }
