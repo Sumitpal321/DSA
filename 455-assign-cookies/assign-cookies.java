4fa126cc83538977920027ffaab9ed57b9338008
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-04 16:57:13 +0530

    Time: 15 ms (60.12%) | Memory: 51.8 MB (6.44%) - LeetSync

--- 455-assign-cookies/assign-cookies.java
+++ 455-assign-cookies/assign-cookies.java
@@ -5,7 +5,7 @@ class Solution {
         Arrays.sort(g);
         Arrays.sort(s);
         int left = 0;
-        int right = 0;;
+        int right = 0;
 
         while(left < n && right < m){
             if(g[right] <= s[left]){
