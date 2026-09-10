cb507756ce8f08eb78bbf0afe186604861e4e67a
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-10 18:51:33 +0530

    Time: 1 ms (27.55%) | Memory: 43.9 MB (6.46%) - LeetSync

---
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int n = s.length();
+        Boolean[][] dp = new Boolean[n][n+1];
+        return solve(s, 0, 0, dp);
+    }
+    public boolean solve(String s, int idx, int count, Boolean[][] dp){
+        int n = s.length();
+        if(count < 0) return false;
+        if(idx == n){
+            return (count == 0);
+        }
+        if(dp[idx][count] != null) return dp[idx][count];
+
+        boolean ans;
+        if(s.charAt(idx) == '('){
+            ans = solve(s, idx+1, count+1, dp);
+        }
+        else if(s.charAt(idx) == ')'){
+            ans = solve(s, idx+1, count-1, dp);
+        }
+        else{
+            boolean open = solve(s, idx+1, count+1, dp);
+            boolean close = solve(s, idx+1, count-1, dp);
+            boolean empty = solve(s, idx+1, count, dp);
+
+            ans = open || close || empty;
+        }
+        dp[idx][count] = ans;
+        return ans;
+    }
+}
