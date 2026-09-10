67bb2ade7cb3346a8facb7841558f607dd27aecf
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-10 21:40:45 +0530

    Time: 1 ms (27.55%) | Memory: 42.9 MB (19.81%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.java
+++ 678-valid-parenthesis-string/valid-parenthesis-string.java
@@ -1,33 +1,35 @@
 class Solution {
     public boolean checkValidString(String s) {
         int n = s.length();
-        boolean[][] t = new boolean[n+1][n+1];
-        t[n][0] = true;
-        for(int i=n-1;i>=0;i--){
-            for(int open=0;open<=n;open++){
-                boolean isValid = false;
-                if(s.charAt(i) == '*'){
-                    if(open<n){
-                        isValid |= t[i+1][open+1];
-                    }
-                    isValid |= t[i+1][open];
-                    if(open>0){
-                        isValid |= t[i+1][open-1];
-                    }
+        Stack<Integer> openSt = new Stack<>();
+        Stack<Integer> asteriskSt = new Stack<>();
+
+        for(int i=0;i<n;i++){
+            if(s.charAt(i) == '('){
+                openSt.push(i);
+            }
+            else if(s.charAt(i) == '*'){
+                asteriskSt.push(i);
+            }
+            else{
+                if(!openSt.isEmpty()){
+                    openSt.pop();
                 }
-                else if(s.charAt(i) == '('){
-                    if(open<n){
-                        isValid |= t[i+1][open+1];
-                    }
+                else if(!asteriskSt.isEmpty()){
+                    asteriskSt.pop();
                 }
                 else{
-                    if(open>0){
-                        isValid |= t[i+1][open-1];
-                    }
+                    return false;
                 }
-                t[i][open] = isValid;
             }
         }
-        return t[0][0];
+        while(!openSt.isEmpty() && !asteriskSt.isEmpty()){
+                if(openSt.peek() > asteriskSt.peek()){
+                    return false;
+                }
+                openSt.pop();
+                asteriskSt.pop();
+            }
+        return openSt.isEmpty();
     }
 }
