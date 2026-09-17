ad311fbb7528c3ff5bc903fbff8e4b896d0aefb1
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-17 10:23:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (99.14%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return solve(s, 0, n, dp);
+    }
+    public int solve(String s, int i, int n, int[] dp){
+        if(i==n) return 1;
+        if(s.charAt(i) == '0') return 0;
+        if(dp[i] != -1) return dp[i];
+        int result = solve(s, i+1, n, dp);
+        if(i+1 < n){
+            if((s.charAt(i)=='1') || (s.charAt(i)=='2' && s.charAt(i+1) <= '6')){
+                result += solve(s, i+2, n, dp);
+            }
+        }
+        return dp[i] = result;
+    }
+}
