aecf451845fb2e69301e52e59d7f8b7a5efb1810
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-18 10:46:05 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (46.05%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -2,21 +2,34 @@ class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
         int m = arr.length;
         int n = arr[0].length;
+        if(arr[0][0] == 1) return 0;
         int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j] = -1;
+        for(int row=0;row<m;row++){
+            if(row >0 && arr[row-1][0] == 1){
+                dp[row][0] = 0;
+                arr[row][0] = 1;
+            }
+            else if(arr[row][0] == 1){
+                dp[row][0] = 0;
+            }
+            else dp[row][0] = 1;
+        }
+        for(int col=0;col<n;col++){
+            if(col>0 && arr[0][col-1] == 1){
+                dp[0][col] = 0;
+                arr[0][col] = 1;
+            }
+            else if(arr[0][col] == 1){
+                dp[0][col] = 0;
+            }
+            else dp[0][col] = 1;
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(arr[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return solve(arr, 0, 0, m, n, dp);
-    }
-    public int solve(int[][] arr, int row, int col, Integer m, Integer n, int[][] dp){
-        if(row>=m || col>=n) return 0;
-        if(arr[row][col] == 1) return 0;
-        if(row == m-1 && col == n-1) return 1;
-        if(dp[row][col] != -1) return dp[row][col];
-        int rightways = solve(arr, row, col+1, m, n, dp);
-        int downways = solve(arr, row+1, col, m , n, dp);
-        return dp[row][col] = rightways + downways;
+        return dp[m-1][n-1];
     }
 }
