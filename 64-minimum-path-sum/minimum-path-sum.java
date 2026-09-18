be87b6985803282e8f8a2f8703499173704838d1
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-18 20:42:34 +0530

    Time: 4 ms (60.23%) | Memory: 50.2 MB (72.35%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.java
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -3,26 +3,21 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j] = -1;
-            }
+        dp[0][0] = grid[0][0];
+
+        for(int j=1;j<n;j++){
+            dp[0][j] = grid[0][j] + dp[0][j-1];
         }
 
-        return solve(grid, 0, 0, m, n, dp);
-    }
-    public int solve(int[][] grid, int row, int col, Integer m, Integer n, int[][] dp){
-        if(row>m || col>n) return 0;
-        if(row == m-1 && col == n-1) return grid[row][col];
-        if(dp[row][col] != -1) return dp[row][col];
-        if(row == m-1){
-            return grid[row][col] + solve(grid, row, col+1, m, n, dp);
+        for(int i=1;i<m;i++){
+            dp[i][0] = grid[i][0] + dp[i-1][0];
         }
-        else if(col == n-1){
-            return grid[row][col] + solve(grid, row+1, col, m, n, dp);
+
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j] = grid[i][j] + Math.min(dp[i][j-1], dp[i-1][j]);
+            }
         }
-        else{
-            return dp[row][col] = grid[row][col] + Math.min(solve(grid, row, col+1, m, n, dp), solve(grid, row+1, col, m, n, dp));
-        }
+        return dp[m-1][n-1];
     }
 }
