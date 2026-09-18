fb70397870d8df2880f38de0bff1f27a924e8cde
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-18 09:49:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (80.79%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,18 +1,22 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] arr) {
-        int m = arr.length, n = arr[0].length;
-        if(arr[0][0]==1) return 0;
-        int[][] dp = new int[m][n];
-        dp[0][0] = 1;
+        int m = arr.length;
+        int n = arr[0].length;
+        int[][] dp = new int[m+1][n+1];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(arr[i][j]==1) dp[i][j] = 0;
-                else{
-                    if(i>0) dp[i][j] += dp[i-1][j];
-                    if(j>0) dp[i][j] += dp[i][j-1];
-                }
+                dp[i][j] = -1;
             }
         }
-        return dp[m-1][n-1];
+        return solve(arr, 0, 0, m, n, dp);
+    }
+    public int solve(int[][] arr, int row, int col, Integer m, Integer n, int[][] dp){
+        if(row>=m || col>=n) return 0;
+        if(arr[row][col] == 1) return 0;
+        if(row == m-1 && col == n-1) return 1;
+        if(dp[row][col] != -1) return dp[row][col];
+        int rightways = solve(arr, row, col+1, m, n, dp);
+        int downways = solve(arr, row+1, col, m , n, dp);
+        return dp[row][col] = rightways + downways;
     }
 }
