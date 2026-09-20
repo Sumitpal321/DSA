ece35ad4a77534521e937378c40aaae0f0a98ab0
Sumit Kumar Pal <Sumitkumar71496@gmail.com>
2026-09-20 16:25:41 +0530

    Time: 7 ms (48.59%) | Memory: 47 MB (79.93%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.java
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -4,40 +4,32 @@ class Solution {
         int n = matrix[0].length;
 
         int[][] dp = new int[m+1][n+1];
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                dp[i][j] = Integer.MAX_VALUE;
+
+        for(int col=0;col<n;col++){
+            dp[0][col] = matrix[0][col];
+        }
+
+        for(int row=1;row<m;row++){
+            for(int col=0;col<n;col++){
+                int a = Integer.MAX_VALUE;
+                int b = Integer.MAX_VALUE;
+
+                if(col-1>=0){
+                    a = dp[row-1][col-1];
+                }
+
+                if(col+1<n){
+                    b = dp[row-1][col+1];
+                }
+
+                dp[row][col] = matrix[row][col] + Math.min(dp[row-1][col], Math.min(a, b));
             }
         }
-        int minSum = Integer.MAX_VALUE;
+        int result = Integer.MAX_VALUE;
+        int lastRow = n-1;
         for(int col=0;col<n;col++){
-            minSum = Math.min(minSum, solve(matrix, 0, col, m, n, dp));
+            result = Math.min(result, dp[lastRow][col]);
         }
-        return minSum;
-    }
-    public int solve(int[][] matrix, int row, int col, Integer m, Integer n, int[][] dp){
-        if(row == m-1) return matrix[row][col];
-        if(dp[row][col] != Integer.MAX_VALUE) return dp[row][col];
-
-        int min = Integer.MAX_VALUE;
-
-        int sum = matrix[row][col];
-
-        int downleft = Integer.MAX_VALUE;
-        if(row+1<m && col - 1>=0){
-            downleft = sum + Math.min(min, solve(matrix, row+1, col-1, m, n, dp));
-        }
-
-        int down = Integer.MAX_VALUE;
-        if(row+1<m){
-            down = sum + Math.min(min, solve(matrix, row+1, col, m, n, dp));
-        }
-
-        int downright = Integer.MAX_VALUE;
-        if(row+1<m && col+1<n){
-            downright = sum + Math.min(min, solve(matrix, row+1, col+1, m, n, dp));
-        }
-
-        return dp[row][col] = Math.min(downleft, Math.min(down, downright));
+        return result;
     }
 }
